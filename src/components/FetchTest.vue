d5e60c8f4d12091d8262d00061206bbdaf5d515d
Michael MacAdam <MikeMac440@gmail.com>
2022-10-27 22:07:41 -0400

    inital commit

---
+++ src/components/FetchTest.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h3>Fetch Test</h3>
+        <p>{{payload.value}}</p>
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const payload = ref({});
+
+function getQuestionList(url){
+   fetch(url, {
+        method: 'GET',
+        mode: 'no-cors',
+        headers: {
+             'Content-Type': 'application/json; charset=utf-8'
+          }
+    })
+    .then((response) => response.json())
+    .then((data) => {
+         console.log(data);
+    })
+    .catch((ex) => {
+        console.log(ex);
+    });
+}
+
+onMounted(() => {
+    getQuestionList('https://localhost:44323/questionlist/1');
+});
+
+</script>
---
+++ src/views/TestView.vue
@@ -0,0 +1,10 @@
+<template>
+    <h2>Hello Vue!!</h2>
+    <BooleanAttr></BooleanAttr>
+    <FetchTest></FetchTest>
+</template>
+
+<script setup>
+import BooleanAttr from '@/components/BooleanAttr.vue';
+import FetchTest from '@/components/FetchTest.vue';
+</script>
